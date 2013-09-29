fd91e20be021ba8bc42c7b2cc0a04df7ae6c8de3
Pauline Ramos <me@paulineramos.ca>
2013-09-29 13:05:39 -0400

    Beta version done

--- mobile-wallet/platforms/android/assets/www/js/index.js
+++ mobile-wallet/platforms/android/assets/www/js/index.js
@@ -16,6 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+var user = null;
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,31 +36,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        // //app.receivedEvent('deviceready');
-        // var xhr = new XMLHttpRequest();
-        // xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
-        // // Response handlers.
-        // xhr.onload = function () {
-        //     var repos = JSON.parse(xhr.response), i, reposHTML = "";
-        //     for (i = 0; i < repos.repositories.length; i++) {
-        //         reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/" + repos.repositories[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
-        //     }
-        //     document.getElementById("allRepos").innerHTML = reposHTML;
-        // };
-
-        // xhr.onerror = function () {
-        //     alert('error making the request.');
-        // };
-
-        // xhr.send();
-
-        // $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
-        //      var i, repo;
-        //      $.each(data.repositories, function (i, repo) {
-        //         $("#allRepos").append("<p><a href='https://github.com/" + repo.username + "/" + repo.name + "'>" + repo.name + "</a><br>"+ repo.description + "&g\lt;/p>");
-        //      });
-        // });
-        //loadRepos();
+        app.receivedEvent('deviceready');
+        nfc.addMimeTypeListener('rc/json', parseReceipt);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -76,153 +56,162 @@ $(document).ready(function () {
     //user sign up
     $("#sign-up-form").submit(function () {
         var data = {
+            method: "register",
             email: $("#sign-up-email").val(),
             firstName: $("#sign-up-firstname").val(),
             lastName: $("#sign-up-lastname").val(),
-            passcode: $("#sign-up-passcode").val()
+            pass: $("#sign-up-passcode").val()
         };
 
-        if (data.email === "" || data.firstname === "" || data.lastname === "" || data.passcode === "") {
-            if (navigator.notification) {
-                navigator.notification.alert("All fields are required.", signUpAlertDismissed);
-            }
-        } else {
-            $.mobile.changePage($("#confirmation"));
+        $.mobile.changePage($("#confirmation"));
 
-            // $.ajax({
-            //     type: "POST",
-            //     url: "",
-            //     cache: false,
-            //     data: JSON.stringify(data),
-            //     contentType: "application/json; charset=utf-8",
-            //     beforeSend: function () {
-            //         $.mobile.showPageLoadingMsg(true);
-            //     },
-            //     complete: function () {
-            //         $.mobile.hidePageLoadingMsg();
-            //     },
-            //     success: function (result) {
-            //         $.mobile.changePage($("#confirmation"));
-            //     },
-            //     error: function (request, error) {
-
-            //     }
-            // });
-        }
+        // if (data.email === "" || data.firstname === "" || data.lastname === "" || data.passcode === "") {
+        //     if (navigator.notification) {
+        //         navigator.notification.alert("All fields are required.", signUpAlertDismissed);
+        //     }
+        // } else {
+        //     $.ajax({
+        //         type: "POST",
+        //         url: "/api",
+        //         data: data,
+        //         beforeSend: function () {
+        //             $.mobile.showPageLoadingMsg(true);
+        //         },
+        //         complete: function () {
+        //             $.mobile.hidePageLoadingMsg();
+        //         },
+        //         success: function (result) {
+        //             if (result.ok) {
+        //                 $.mobile.changePage($("#confirmation"));
+        //             } else if (result.error) {
+        //                 if (navigator.notification) {
+        //                     navigator.notification.alert(result.error, signUpAlertDismissed);
+        //                 }
+        //             } else {
+        //                 if (navigator.notification) {
+        //                     navigator.notification.alert("An error occurred", signUpAlertDismissed);
+        //                 }
+        //             }
+        //         },
+        //         error: function (request, error) {
+        //         }
+        //     });
+        // }
     });
 
     //login
     $("#login-form").submit(function () {
         var data = {
+            method: "login",
             email: $("#login-email").val(),
-            passcode: $("#login-passcode").val()
+            pass: $("#login-passcode").val()
         };
 
-        if (data.email === "" || data.passcode === "") {
-            if (navigator.notification) {
-                navigator.notification.alert("All fields are required are required.", loginAlertDismissed);
-            }
-        } else {
-            $.mobile.changePage($("#wallet"));
+        $.mobile.changePage($("#wallet"));
 
-            // $.ajax({
-            //     type: "POST",
-            //     url: "",
-            //     data: data,
-            //     ,
-            //     beforeSend: function () {
-            //         $.mobile.showPageLoadingMsg(true);
-            //     },
-            //     complete: function () {
-            //         $.mobile.hidePageLoadingMsg();
-            //     },
-            //     success: function (result) {
-            //         $.mobile.changePage($("#wallet"));
-            //     },
-            //     error: function (request, error) {
-            //     }
-            // });
-        }
+        // if (data.email === "" || data.passcode === "") {
+        //     if (navigator.notification) {
+        //         navigator.notification.alert("All fields are required are required.", loginAlertDismissed);
+        //     }
+        // } else {
+        //     $.ajax({
+        //         type: "POST",
+        //         url: "api",
+        //         data: data,
+        //         beforeSend: function () {
+        //             $.mobile.showPageLoadingMsg(true);
+        //         },
+        //         complete: function () {
+        //             $.mobile.hidePageLoadingMsg();
+        //         },
+        //         success: function (result) {
+        //             if (result.error) {
+        //                 if (navigator.notification) {
+        //                     navigator.notification.alert(result.error, loginAlertDismissed);
+        //                 }
+        //             } else {
+        //                 user = result;
+        //                 $.mobile.changePage($("#wallet"));
+        //             }
+        //         },
+        //         error: function (request, error) {
+        //         }
+        //     });
+        //}
     });
 
     //add card
     $("#add-card-form").submit(function () {
-        var data = {
-            mastercard: $("#add-card-mastercard").val(),
-            cardholder: $("#add-card-cardholder").val(),
-            expiration: $("#add-card-month").val() + $("#add-card-year").val(),
-            cvv: $("#add-card-cvv").val()
-        }
+        // var data = {
+        //     method: "addCreditCard",
+        //     number: $("#add-card-mastercard").val(),
+        //     nameOnCard: $("#add-card-cardholder").val(),
+        //     expiry: $("#add-card-month").val() + $("#add-card-year").val(),
+        //     ccid: $("#add-card-cvv").val()
+        // }
 
-        if (data.mastercard === "" || data.cardholder === "" || data.expiration === "" || data.cvv === "") {
-            if (navigator.notification) {
-                navigator.notification.alert("All fields are required are required.", addCardAlertDismissed);
-            }
-        } else {
-            $.mobile.changePage($("#wallet"));
-
-            // $.ajax({
-            //     type: "POST",
-            //     url: "",
-            //     data: data,
-            //     ,
-            //     beforeSend: function () {
-            //         $.mobile.showPageLoadingMsg(true);
-            //     },
-            //     complete: function () {
-            //         $.mobile.hidePageLoadingMsg();
-            //     },
-            //     success: function (result) {
-            //         $.mobile.changePage($("#wallet"));
-            //     },
-            //     error: function (request, error) {
-            //     }
-            // });
-        }
+        // if (data.mastercard === "" || data.cardholder === "" || data.expiration === "" || data.cvv === "") {
+        //     if (navigator.notification) {
+        //         navigator.notification.alert("All fields are required are required.", addCardAlertDismissed);
+        //     }
+        // } else {
+        //     $.ajax({
+        //         type: "POST",
+        //         url: "api",
+        //         data: data,
+        //         beforeSend: function () {
+        //             $.mobile.showPageLoadingMsg(true);
+        //         },
+        //         complete: function () {
+        //             $.mobile.hidePageLoadingMsg();
+        //         },
+        //         success: function (result) {
+        //             $.mobile.changePage($("#wallet"));
+        //         },
+        //         error: function (request, error) {
+        //         }
+        //     });
+        // }
     });
 
     //accept trupass
     $("#accept-trupass-form").submit(function () {
-        var data = {
-            answer: $("#security-answer").val()
-        };
+        // var data = {
+        //     answer: $("#security-answer").val()
+        // };
         
-        if (data.answer === "") {
-            if (navigator.notification) {
-                navigator.notification.alert("All fields are required are required.", addCardAlertDismissed);
-            }
-        } else {
-            $.mobile.changePage($("#wallet"));
+        // if (data.answer === "") {
+        //     if (navigator.notification) {
+        //         navigator.notification.alert("All fields are required are required.", addCardAlertDismissed);
+        //     }
+        // } else {
+        //     $.mobile.changePage($("#wallet"));
 
-            // $.ajax({
-            //     type: "POST",
-            //     url: "",
-            //     data: data,
-            //     ,
-            //     beforeSend: function () {
-            //         $.mobile.showPageLoadingMsg(true);
-            //     },
-            //     complete: function () {
-            //         $.mobile.hidePageLoadingMsg();
-            //     },
-            //     success: function (result) {
-            //         $.mobile.changePage($("#wallet"));
-            //     },
-            //     error: function (request, error) {
-            //     }
-            // });
-        }
+        //     $.ajax({
+        //         type: "POST",
+        //         url: "",
+        //         data: data,
+        //         beforeSend: function () {
+        //             $.mobile.showPageLoadingMsg(true);
+        //         },
+        //         complete: function () {
+        //             $.mobile.hidePageLoadingMsg();
+        //         },
+        //         success: function (result) {
+        //             $.mobile.changePage($("#wallet"));
+        //         },
+        //         error: function (request, error) {
+        //         }
+        //     });
+        // }
     });
 
     //account confirmation
     $("#confirmation-form").submit(function () {
-        $.mobile.changePage($("#enter-passcode"));
-
         // $.ajax({
         //     type: "POST",
-        //     url: "",
+        //     url: "api",
         //     data: data,
-        //     ,
         //     beforeSend: function () {
         //         $.mobile.showPageLoadingMsg(true);
         //     },
@@ -239,35 +228,48 @@ $(document).ready(function () {
 
     //enter passcode
     $("#enter-passcode-form").submit(function () {
-        var data = {
-            passcode: $("#enter-passcode-passcode").val()
-        };
+        // var data = {
+        //     passcode: $("#enter-passcode-passcode").val()
+        // };
 
-        if (data.passcode === "") {
-            if (navigator.notification) {
-                navigator.notification.alert("All fields are required are required.", enterPasscodeAlertDismissed);
-            }
-        } else {
-            $.mobile.changePage($("#wallet"));
+        // if (data.passcode === "") {
+        //     if (navigator.notification) {
+        //         navigator.notification.alert("All fields are required are required.", enterPasscodeAlertDismissed);
+        //     }
+        // } else {
+        //     $.ajax({
+        //         type: "POST",
+        //         url: "",
+        //         data: data,
+        //         beforeSend: function () {
+        //             $.mobile.showPageLoadingMsg(true);
+        //         },
+        //         complete: function () {
+        //             $.mobile.hidePageLoadingMsg();
+        //         },
+        //         success: function (result) {
+        //             $.mobile.changePage($("#wallet"));
+        //         },
+        //         error: function (request, error) {
+        //         }
+        //     });
+        // }
+    });
 
-            // $.ajax({
-            //     type: "POST",
-            //     url: "",
-            //     data: data,
-            //     ,
-            //     beforeSend: function () {
-            //         $.mobile.showPageLoadingMsg(true);
-            //     },
-            //     complete: function () {
-            //         $.mobile.hidePageLoadingMsg();
-            //     },
-            //     success: function (result) {
-            //         $.mobile.changePage($("#wallet"));
-            //     },
-            //     error: function (request, error) {
-            //     }
-            // });
-        }
+    $("#enable-nfc").click(function (e) {
+        e.preventDefault();
+        
+        $(".nfc-disabled").hide();
+        $(".nfc-enabled").show();
+        shareTag();
+    });
+
+    $("#send-nfc-data").click(function (e) {
+        e.preventDefault();
+        
+        $(".nfc-enabled").hide();
+        $(".nfc-disabled").show();
+        unshareTag();
     });
 });
 
@@ -287,80 +289,38 @@ function enterPasscodeAlertDismissed() {
     $mobile.changePage("#enter-passcode");
 }
 
-function loadMyCards() {
-    // $.ajax("").done(function (data) {
-    //     var idx, card;
-
-    //     $.each(data.cards, function (idx, card) {
-    //         $("#my-cards").append("<li></li>");
-    //     });
-    //     $("#my-cards").listview("refresh");
-    // });
+function unshareTag() {
+    nfc.unshare(
+        function () {
+            navigator.notification.vibrate(100);
+            toast.showShort("Tag is no longer shared");
+        }, function (reason) {
+            navigator.notification.alert("Failed to unshare tag " + reason);
+        });
 }
 
-function loadMyTruPass() {
-    // $.ajax("").done(function (data) {
-    //     var idx, trupass;
+function shareTag() {
+    var mimeType = "cc/json",
+        payload = '{ "cardNumber": "5113402247971907", "expMonth": "03", "expYear": "18", "cvc": "177" }',
+        record = ndef.mimeMediaRecord(mimeType, nfc.stringToBytes(payload));
 
-    //     $.each(data.cards, function (idx, trupass) {
-    //         $("#my-trupass").append("<li></li>");
-    //     });
-    //     $("#my-trupass").listview("refresh");
-    // });
+    nfc.share(
+        [record],
+        function () {
+            navigator.notification.vibrate(100);
+            toast.showShort("Sharing Tag");
+        }, function (reason) {
+            navigator.notification.alert("Failed to share tag " + reason);
+            // when NDEF_PUSH_DISABLED, open setting and enable Android Beam
+        });
 }
 
+function parseReceipt(receipt) {
+    navigator.notification.alert("You've been charged $10.00");
+}
+
+// Load User Wallet
 $(document).on("pageshow", "#wallet", function (e) {
-    loadMyCards();
-    loadMyTruPass();
+    //console.log('wallet page');
+    //console.log(user);
 });
-
-
-// $('#reposHome').bind('pageinit', function(event) {
-//     loadRepos();
-// });
-
-// function loadRepos() {
-//     $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
-//         var i, repo;
-//         $.each(data.repositories, function (i, repo) {
-//             $("#allRepos").append("<li><a href='repo-detail.html?owner=" + repo.username + "&name=" + repo.name + "'>"
-//             + "<h4>" + repo.name + "</h4>"
-//             + "<p>" + repo.username + "</p></a></li>");
-//         });
-//         $('#allRepos').listview('refresh');
-//     });
-// }
-
-
-// function getUrlVars() {
-//     var vars = [], hash;
-//     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-//     for(var i = 0; i < hashes.length; i++)
-//     {
-//         hash = hashes[i].split('=');
-//         vars.push(hash[0]);
-//         vars[hash[0]] = hash[1];
-//     }
-//     return vars;
-// }
-
-
-// $(document).on("pageshow", "#reposDetail", function(event) {
-//     var owner = getUrlVars().owner;
-//     var name = getUrlVars().name;
-//     loadRepoDetail(owner,name);
-// });
-
-// function loadRepoDetail(owner,name) {
-//      $.ajax("https://api.github.com/repos/" + owner + "/" + name).done(function(data) {
-//          //console.log(data);
-//          var repo = data;
-
-//         $('#repoName').html("<a href='" + repo.homepage + "'>" + repo.name + "</a>");
-//         $('#description').text(repo.description);
-//         $('#forks').html("<strong>Forks:</strong> " + repo.forks + "<br><strong>Watchers:</strong> " + repo.watchers);
-
-//         $('#avatar').attr('src', repo.owner.avatar_url);
-//         $('#ownerName').html("<strong>Owner:</strong> <a href='" + repo.owner.url + "'>" + repo.owner.login + "</a>");
-//      });
-// }
